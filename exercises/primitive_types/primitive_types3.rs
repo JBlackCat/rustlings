b2d333270c0c2d82e4d6945d071a479e04b1eb8a
JBlackCat <placeholder.email.com>
2021-11-28 20:56:56 -0800

    fix: complete variables through enum exercises

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,13 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a: [i32; 500] = [0; 500];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
+        // println!("second element of the array: {}", a[1]);
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
